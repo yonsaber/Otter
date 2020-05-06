ea1cc5b3636ed7d5ab3a5c74bec3cb8555cf5be9
riley <riley@hidinginthevoid.com>
2020-05-06 23:41:43 +0100

    Example: Setup toggle in the PlatformingMovement project to show off changes
    Demo so you can toggle between the two versions of Ogmo with ease

--- Examples/PlatformingMovement/PlatformScene.cs
+++ Examples/PlatformingMovement/PlatformScene.cs
@@ -1,14 +1,41 @@
 using Otter.Core;
+using Otter.Graphics.Text;
 using Otter.Utility;
+using System;
+using System.Linq;
 
 namespace PlatformingExample
 {
     class PlatformerScene : Scene
     {
+        OgmoProject.OgmoVersion _version;
+        Text _versionText;
+
         public PlatformerScene() : base()
         {
+            _version = OgmoProject.OgmoVersion.Version2;
+            Load(_version);
+        }
+
+        void Load(OgmoProject.OgmoVersion version)
+        {
             // Create the Ogmo Editor project.
-            var ogmoProject = new OgmoProject("OgmoProject.oep");
+            OgmoProject ogmoProject;
+
+            _version = version;
+
+            if (_version == OgmoProject.OgmoVersion.Version2)
+            {
+                ogmoProject = new OgmoProject("OgmoProject.oep");
+            }
+            else if (_version == OgmoProject.OgmoVersion.Version3)
+            {
+                ogmoProject = new OgmoProject("Ogmo3Project.ogmo");
+            }
+            else
+            {
+                throw new Exception();
+            }
 
             // Register the "Solid" layer with the tag Solid.
             ogmoProject.RegisterTag(CollisionTag.Solid, "Solid");
@@ -17,7 +44,48 @@ namespace PlatformingExample
             Game.Instance.Color = ogmoProject.BackgroundColor;
 
             // Load the level.
-            ogmoProject.LoadLevel("Level.oel", this);
+            if (_version == OgmoProject.OgmoVersion.Version2)
+            {
+                ogmoProject.LoadLevel("Level.oel", this);
+            }
+            else if (_version == OgmoProject.OgmoVersion.Version3)
+            {
+                ogmoProject.LoadLevel("Level.json", this);
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            _versionText = new Text(_version == OgmoProject.OgmoVersion.Version2 ? "Version 2 (XML)" : "Version 3 (JSON)", 16);
+        }
+
+        public override void Update()
+        {
+            if (Input.KeyPressed(Key.K))
+            {
+                if (_version == OgmoProject.OgmoVersion.Version2)
+                    return;
+
+                RemoveAll();
+                Load(OgmoProject.OgmoVersion.Version2);
+            }
+
+            if (Input.KeyPressed(Key.L))
+            {
+                if (_version == OgmoProject.OgmoVersion.Version3)
+                    return;
+
+                RemoveAll();
+                Load(OgmoProject.OgmoVersion.Version3);
+            }
+        }
+
+        public override void Render()
+        {
+            _versionText.Render(10, 10);
+
+            base.Render();
         }
     }
 }
